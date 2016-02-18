e7e3b63015735b8a2d36c222d6e2e597a41a80b8
Jessica Hathaway <hathaway.jess@gmail.com>
2016-02-17 20:29:01 -0700

    Completed main project

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 var app = angular.module('chatroom', []);
 
+angular.module('chatroom').filter('format', function() {
+	return function(date) {
+		return moment(date).format('ddd, h' + ':' + 'mmA');
+	}
+});
+
 app.config(function($httpProvider){
   $httpProvider.interceptors.push('httpRequestInterceptor');
 });
--- js/parseService.js
+++ js/parseService.js
@@ -12,11 +12,24 @@ app.service('parseService', function($http){
   //On the line below create the postData method. This method will add data to the parse backend.
   //The url for the request needs to be 'https://api.parse.com/1/classes/chat'
   //Because we're making a POST request, we need a way to tell parse the data we want to give it, 
-    //in your $http call (along with url and method) have a data property which has a value that is equal to another object which a key of text and a value of the message being passed to parse. IE data: {text: yourMessage} 
+    //in your $http call (along with url and method) have a data property which has a value that is equal to another object which a key of text 
+    //and a value of the message being passed to parse. IE data: {text: yourMessage} 
   //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
   
   //postData method here
-  
+  this.postData = function(message) {
+    return $http({ // Requests that your profile be added to the database
+          method: 'POST'
+        , url: 'https://api.parse.com/1/classes/chat'
+        , data: {text: message}
+      });
+  }
   
   //getData method here
+  this.getData = function() {
+    return $http({ // Requests that your profile be added to the database
+          method: 'GET'
+        , url: 'https://api.parse.com/1/classes/chat?order=-createdAt'
+      });
+  }
 });
